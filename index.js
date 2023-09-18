64d6836ee85eb356f851f2cc4877ae8d0ab9d532
PrameKittikorn ✅ <100893808+PrameKittikorn@users.noreply.github.com>
2023-09-18 06:15:04 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const req = require('express/lib/request');
+const app = express();
+const port = 3000;
+
+app.set("view engine","ejs");
+
+const products = [];
+
+for (let i = 1; i <= 100; i++) {
+  const product = {
+    id: i,
+    name: `Product ${i}`,
+    price: (Math.random() * 100).toFixed(2),
+    description: `This is a description for Product ${i}`
+  };
+  products.push(product);
+}
+
+app.get('/products',(req,res) => {
+  const limit = parseInt(req.query.limit) || 10 ;
+  const page = parseInt(req.query.page) || 1 ;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const paginatedProducts = products.slice(startIndex, endIndex);
+  console.log(paginatedProducts);
+
+  res.render("products",{paginatedProducts,limit,page});
+
+});
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  res.render("index")
+});
+
+app.get('/page2',(req,res) => {
+  const name = req.query.name;
+  const age = req.query.age;
+  res.render("page2", {name, age})
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
